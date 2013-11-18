a0e07bbb5bcae485d2d5da54940fb30f44b0aea0
Gabriel Paladino <gabriel@scupen.com>
2013-11-18 14:47:59 -0200

    nova url testes

--- lib/superpay/configuracao.rb
+++ lib/superpay/configuracao.rb
@@ -14,8 +14,8 @@ module Superpay
         return 'https://superpay2.superpay.com.br/checkout/servicosPagamentoOneClickWS.Services?wsdl'
         # return 'https://superpay2.superpay.com.br/checkout/servicosPagamentoCompletoWS.Services?wsdl'
       else
-        return 'http://homologacao2.superpay.com.br/checkout/servicosPagamentoOneClickWS.Services?wsdl'
-        # return 'http://homologacao2.superpay.com.br/checkout/servicosPagamentoCompletoWS.Services?wsdl'
+        return 'http://homologacao2.superpay.com.br/superpay/servicosPagamentoOneClickWS.Services?wsdl'
+        # return 'http://homologacao2.superpay.com.br/superpay/servicosPagamentoCompletoWS.Services?wsdl'
       end
     end
 
